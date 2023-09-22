35cd5bf9fb1d55eb8bd5773423e960a7ef414f56
Apple <appleeyespackage@gmail.com>
2023-09-22 18:36:38 +0100

    feat: create migration tables

--- database/migrations/2023_09_22_170103_create_follows_table.php
+++ database/migrations/2023_09_22_170103_create_follows_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('follower_id');
+            $table->unsignedBigInteger('following_id');
             $table->timestamps();
+            $table->foreign('follower_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('following_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
--- database/migrations/2023_09_22_170210_create_notifications_table.php
+++ database/migrations/2023_09_22_170210_create_notifications_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id'); 
+            $table->string('type');
+            $table->unsignedBigInteger('notifiable_id');
+            $table->string('notifiable_type');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->index(['notifiable_id', 'notifiable_type']);
         });
     }
 
