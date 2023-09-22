bce473df0b8d3f7dfa095cc11bae168e0bfacd51
Apple <appleeyespackage@gmail.com>
2023-09-22 18:03:31 +0100

    Initial commit

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notifications;
+use Illuminate\Http\Request;
+
+class NotificationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Notifications $notifications)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Notifications $notifications)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Notifications $notifications)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Notifications $notifications)
+    {
+        //
+    }
+}
